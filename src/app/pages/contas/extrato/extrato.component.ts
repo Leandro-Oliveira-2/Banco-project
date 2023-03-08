7f73ddf7ec328cc4181757976820ea14afe10c21
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2023-03-08 15:45:43 -0300

    add extrato screen and css

--- src/app/pages/contas/extrato/extrato.component.ts
+++ src/app/pages/contas/extrato/extrato.component.ts
@@ -1,4 +1,11 @@
+import { ContasService } from 'src/app/services/contas.service';
+import { IextratoPeriodo } from './../../../interfaces/extratoPeriodo';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IExtrato } from './../../../interfaces/extrato';
+import { ExtratoService } from './../../../services/extrato.service';
 import { Component, OnInit } from '@angular/core';
+import { AlertasService } from 'src/app/services/alertas.service';
 
 @Component({
   selector: 'app-extrato',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ContasService: ContasService,
+    private extratoService: ExtratoService,
+    private route: ActivatedRoute,
+    private alertaService: AlertasService,
+    private router: Router
+    ) { }
 
+
+  transacoes: IExtrato[] = [];
+
+  extratoForm = new FormGroup({
+    agencia: new FormControl('', Validators.required),
+    numero: new FormControl('', Validators.required),
+    dataInicial: new FormControl('', Validators.required),
+    dataFinal: new FormControl('', Validators.required),
+  })
   ngOnInit(): void {
+  }
+
+  listarTransacoesPorPeriodo() {
+    const extratoPeriodo: IextratoPeriodo = this.extratoForm.value as IextratoPeriodo;
+    this.extratoService.listarTransacoesPorPeriodo(extratoPeriodo).subscribe((extrato: IExtrato[]) => {
+      this.transacoes = extrato;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  buscarClientePorAgencia(){
+
   }
 
 }
--- src/app/pages/contas/saque/saque.component.ts
+++ src/app/pages/contas/saque/saque.component.ts
@@ -1,3 +1,7 @@
+import { AlertasService } from 'src/app/services/alertas.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { saqueService } from './../../../services/saque.service';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaqueComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private saqueService: saqueService,
+    private Router: ActivatedRoute,
+    private alertaService: AlertasService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
+  sacarForm = new FormGroup({
+    agencia: new FormControl('', Validators.required),
+    numeroConta: new FormControl('', Validators.required),
+    valor: new FormControl('', Validators.required)
+  })
+
+  sacar(){
+    const saque = {
+      agencia: this.sacarForm.value.agencia,
+      numeroConta: this.sacarForm.value.numeroConta,
+      valor: Number(this.sacarForm.value.valor)
+    }
+    this.saqueService.sacar(saque).subscribe(()=>{
+      this.alertaService.alertaSucesso('Parabéns, saque concluído com sucesso!');
+      this.router.navigateByUrl('/clientes');
+    });
+  }
+
 
 }
